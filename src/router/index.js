49f286bae72740a680389cd2a8d97fb3d9345223
NKDShinKu <2010182879@qq.com>
2024-09-21 13:24:00 +0800

    perf:路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Layout from '@/views/layout'
-import Login from '@/views/login'
-import MyOrder from '@/views/myorder'
-import Pay from '@/views/pay'
-import Prodetail from '@/views/prodetail'
-import Search from '@/views/search/index.vue'
-import SearchList from '@/views/search/list'
 import Home from '@/views/layout/home'
 import Category from '@/views/layout/category'
 import Shopping from '@/views/layout/shopping'
 import User from '@/views/layout/user'
+
 import store from '@/store/index.js'
+
+// 路由懒加载
+const Login = () => import('@/views/login')
+const MyOrder = () => import('@/views/myorder')
+const Pay = () => import('@/views/pay')
+const Prodetail = () => import('@/views/prodetail')
+const Search = () => import('@/views/search/index.vue')
+const SearchList = () => import('@/views/search/list')
 
 Vue.use(VueRouter)
 
